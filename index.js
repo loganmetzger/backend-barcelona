13eb420dfbe77acc16fc8b122c92a80b9263bdb4
Harrison <hmseaborn@gmail.com>
2021-01-29 13:10:20 -0800

    knexfile touched up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
-const server = require('./api/server.js');
+require("dotenv").config();
+const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 6000;
 server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+	console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
