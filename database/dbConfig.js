a940741b6952a35d4262360df6006be06376ea13
Harrison <hmseaborn@gmail.com>
2021-01-29 13:17:48 -0800

    attempting node modules deletion

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const knex = require("knex");
-=======
-const knex = require('knex')('production')
->>>>>>> f185a8a66dac13dc43bf53533f79379f8305c02d
 
 const knexConfig = require("../knexfile.js");
 
